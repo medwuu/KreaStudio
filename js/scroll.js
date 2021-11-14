48bd2b291e7fd09df300daaf5c9f411ff353c157
medwuu <91782808+medwuu@users.noreply.github.com>
2021-11-14 14:52:11 +0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -8,11 +8,17 @@ function scrollTo(element) {
 
 var aboutBtn = document.querySelector('#about_btn');
 var about = document.querySelector('#about');
+var reviewBtn = document.querySelector('#review_btn');
+var review = document.querySelector('#review');
 var contactsBtn = document.querySelector('#contacts_btn');
 var contacts = document.querySelector('#contacts');
 
 aboutBtn.addEventListener('click', () => {
     scrollTo(about);
+})
+
+reviewBtn.addEventListener('click', () => {
+    scrollTo(review);
 })
 
 contactsBtn.addEventListener('click', () => {
